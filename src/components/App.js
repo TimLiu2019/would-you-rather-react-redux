ab68da4d7f5a45f92e00c27542787a4e27fb032e
TimLiu2019 <qitingliu7@gmail.com>
2021-05-11 11:39:24 -0400

    using hooks at App

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.dispatch(handleInitialData());
-  }
-  render() {
-    return <div>Starter Code</div>;
-  }
-}
+const App = props => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  });
+
+  return <div>Starter Code</div>;
+};
+
 
 export default connect()(App);
