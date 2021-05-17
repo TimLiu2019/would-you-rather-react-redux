d45b5b55d90abee9daa968a5be6931d9dfbecd30
TimLiu2019 <qitingliu7@gmail.com>
2021-05-17 14:46:21 -0400

    new question

--- src/components/newQuestion/NewQuestion.jsx
+++ src/components/newQuestion/NewQuestion.jsx
@@ -1,19 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import { useHistory } from 'react-router-dom'
 import Button from "react-bootstrap/Button";
+import { handleAddQuestion } from "../../actions/questions";
 
 const NewQuestion = props => {
+  const [optionOne, setOptionOne] = useState("");
+  const [optionTwo, setOptionTwo] = useState("");
+  const history = useHistory();
   useEffect(() => {});
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { dispatch } = props;
+    dispatch(handleAddQuestion(optionOne, optionTwo));
+
+    console.log("option one", optionOne);
+    console.log("option one", optionTwo);
+    setOptionOne("");
+    setOptionTwo("");
+    history.push('/');
+  };
 
   return (
     <div className="new-question">
       <h4 className="center">Create New Question</h4>
       <p>Complete the question</p>
-      <p className='new-question-title'>Would you rather...</p>
-      <input type="text" placeholder="Enter Option one text here" />
+      <p className="new-question-title">Would you rather...</p>
+      <input
+        type="text"
+        placeholder="Enter Option one text here"
+        onChange={e => setOptionOne(e.target.value)}
+      />
       <p className="center new-question-title"> Or</p>
-      <input type="text" placeholder="Enter Option two text here" />
-      <Button variant="outline-primary" >
+      <input
+        type="text"
+        placeholder="Enter Option two text here"
+        onChange={e => setOptionTwo(e.target.value)}
+      />
+      <Button
+        variant="outline-primary"
+        onClick={handleSubmit}
+        disabled={optionOne === "" || optionTwo === ""}
+      >
         Submit
       </Button>
     </div>
