9f178f341e4f4a4f0cddf63ded8894e9e8de12b6
TimLiu2019 <qitingliu7@gmail.com>
2021-05-17 08:33:04 -0400

    new question layout

--- src/components/newQuestion/NewQuestion.jsx
+++ src/components/newQuestion/NewQuestion.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-
+import Button from "react-bootstrap/Button";
 
 const NewQuestion = props => {
-  useEffect(() => {
-   
-  });
+  useEffect(() => {});
 
-  return <div>NewQuestion</div>;
+  return (
+    <div className="new-question">
+      <h4 className="center">Create New Question</h4>
+      <p>Complete the question</p>
+      <p className='new-question-title'>Would you rather...</p>
+      <input type="text" placeholder="Enter Option one text here" />
+      <p className="center new-question-title"> Or</p>
+      <input type="text" placeholder="Enter Option two text here" />
+      <Button variant="outline-primary" >
+        Submit
+      </Button>
+    </div>
+  );
 };
-
 
 export default connect()(NewQuestion);
